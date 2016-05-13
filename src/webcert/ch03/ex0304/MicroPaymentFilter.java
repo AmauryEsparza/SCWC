5d3e9cc07db655a81b7d50a52dfc9589afae83d5
Amaury Esparza <amauryesparza@gmail.com>
2016-05-13 00:11:40 -0500

    Exercise 0304 using filters

--- src/webcert/ch03/ex0304/MicroPaymentFilter.java
+++ src/webcert/ch03/ex0304/MicroPaymentFilter.java
@@ -1,7 +1,28 @@
 package webcert.ch03.ex0304;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * Created by AmauryEsparza on 5/12/16.
  */
-public class MicroPaymentFilter {
+public class MicroPaymentFilter implements Filter{
+
+    private FilterConfig filterConfig = null;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        request.setAttribute("referrer", request.getLocalAddr());
+        filterChain.doFilter(request, response);
+        response.getWriter().println("Micropayment completed");
+    }
+
+    @Override
+    public void destroy() {
+        this.filterConfig = null;
+    }
 }
--- src/webcert/ch03/ex0304/MicroPaymentServlet.java
+++ src/webcert/ch03/ex0304/MicroPaymentServlet.java
@@ -3,6 +3,7 @@ package webcert.ch03.ex0304;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by AmauryEsparza on 5/12/16.
@@ -10,7 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class MicroPaymentServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String reffererName = (String) request.getAttribute("refferer");
+        response.getWriter().println("Request param refferer: " + reffererName);
     }
 }
