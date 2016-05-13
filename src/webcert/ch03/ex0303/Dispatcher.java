6607868c38b61a7acea5e0b397b87642bc5fa2ce
Amaury Esparza <amauryesparza@gmail.com>
2016-05-12 20:35:11 -0500

    Exercise 3.3
    The forward and the include, all the parameter passed through the request are accesibles for all the forwarded or included files. 
    The difference between this two methods is that the forward method, switch to the other servlet and the include add this servlet to the called servlet.

--- src/webcert/ch03/ex0303/Dispatcher.java
+++ src/webcert/ch03/ex0303/Dispatcher.java
@@ -1,8 +1,11 @@
 package webcert.ch03.ex0303;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by AmauryEsparza on 5/11/16.
@@ -10,7 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 public class Dispatcher extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mode = request.getParameter("mode");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/ex0303/Receiver?fruit=orange");
 
+        if("forward".equals(mode)){
+            dispatcher.forward(request, response);
+        }else if("include".equals(mode)){
+            dispatcher.include(request, response);
+        }
+
+        response.getWriter().println(mode + " processed");
     }
 }
--- src/webcert/ch03/ex0303/Receiver.java
+++ src/webcert/ch03/ex0303/Receiver.java
@@ -1,7 +1,25 @@
 package webcert.ch03.ex0303;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 /**
  * Created by AmauryEsparza on 5/11/16.
  */
-public class Receiver {
+public class Receiver extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        PrintWriter out = response.getWriter();
+        Enumeration paramNames = request.getParameterNames();
+        while(paramNames.hasMoreElements()){
+            String paramName = (String) paramNames.nextElement();
+            out.println("Parameter " + paramNames + " value " + request.getParameter(paramName));
+        }
+    }
 }
