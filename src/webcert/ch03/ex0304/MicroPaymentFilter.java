000c749339ee9583871fb4f7b26c41b9d89abb5c
Amaury Esparza <amauryesparza@gmail.com>
2016-05-13 00:08:59 -0500

    Exercise 3.4
    Using the Filters

---
+++ src/webcert/ch03/ex0304/MicroPaymentFilter.java
@@ -0,0 +1,7 @@
+package webcert.ch03.ex0304;
+
+/**
+ * Created by AmauryEsparza on 5/12/16.
+ */
+public class MicroPaymentFilter {
+}
---
+++ src/webcert/ch03/ex0304/MicroPaymentServlet.java
@@ -0,0 +1,16 @@
+package webcert.ch03.ex0304;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by AmauryEsparza on 5/12/16.
+ */
+public class MicroPaymentServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
