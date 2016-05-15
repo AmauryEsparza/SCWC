db81543ab32af088a98b6b3ab6cd967253476dce
Amaury Esparza <amauryesparza@gmail.com>
2016-05-15 12:17:40 -0500

    Exerice 4.1
    Re-uploading

--- src/webcert/ch04/ex0401/SessionDisplayer.java
+++ src/webcert/ch04/ex0401/SessionDisplayer.java
@@ -1,7 +1,34 @@
 package webcert.ch04.ex0401;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by AmauryEsparza on 5/15/16.
  */
-public class SessionDisplayer {
+public class SessionDisplayer extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if(request.getParameter("getSession") != null && request.getParameter("getSession").equals("true")) {
+            HttpSession session = request.getSession();
+            if (session.isNew()) {
+                session.setAttribute("accesses", 1);
+            } else {
+                int accessesCounter = (int) session.getAttribute("accesses");
+                session.setAttribute("accesses", ++accessesCounter);
+            }
+            out.println("Number of accessed sessions " + session.getAttribute("accesses") + " Length " + (System.currentTimeMillis() - session.getCreationTime()));
+        }else{
+            out.println("No getSessionParameter found");
+        }
+
+    }
 }
